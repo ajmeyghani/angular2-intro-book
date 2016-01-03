117937d18d2477d23068b465a9ca2cee1516b198
Amin Meyghani <meyghania@gmail.com>
2016-01-03 00:25:48 -0500

    update build task to copy images.

--- build.sh
+++ build.sh
@@ -2,6 +2,14 @@
 # build each chapter, convert each chapter to pdf
 # build the book in all formats
 rm -rf output
+rm -rf images
+# copy all the images to output/images
+mkdir -p output/images
+mkdir -p images
+find -E chapters -regex ".*\.(jpg|gif|png|jpeg)" -exec cp {} ./output/images/ \;
+find -E chapters -regex ".*\.(jpg|gif|png|jpeg)" -exec cp {} ./images/ \;
+# build
 bookly build -e
 bookly build -a
 bookly build -r
+
